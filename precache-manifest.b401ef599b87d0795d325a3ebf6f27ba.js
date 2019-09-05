fcb6a66de949b05c9454e392b9fa872a08451900
saltowl <vladislava-shakirova@mail.ru>
2019-09-05 12:50:08 +0300

    Updates

---
+++ precache-manifest.b401ef599b87d0795d325a3ebf6f27ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e47ad54a4f5beddd5e4f6e974607816",
+    "url": "/SimpleCalculator/index.html"
+  },
+  {
+    "revision": "a300a3cdb56c75428421",
+    "url": "/SimpleCalculator/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "374def11dd667d2a7516",
+    "url": "/SimpleCalculator/static/css/main.95e914ab.chunk.css"
+  },
+  {
+    "revision": "a300a3cdb56c75428421",
+    "url": "/SimpleCalculator/static/js/2.d7202fa2.chunk.js"
+  },
+  {
+    "revision": "374def11dd667d2a7516",
+    "url": "/SimpleCalculator/static/js/main.50eef383.chunk.js"
+  },
+  {
+    "revision": "23e860149b8a8a9f045d",
+    "url": "/SimpleCalculator/static/js/runtime~main.57a97d72.js"
+  }
+]);
